24a0c780a26c2104df1995513b05d362745e39da
BadBrahmin <tejasshree95@gmail.com>
2019-07-17 17:18:48 +0530

    add backgroundChanger

--- assignments/events/2. backgroundChange/main.js
+++ assignments/events/2. backgroundChange/main.js
@@ -1,12 +1,25 @@
 var button = document.querySelector("button");
 
 var bgColor = document.body.style.background;
-document.body.addEventListener("click", change);
+document.addEventListener("mousedown", change);
 
-function change() {
+function change(e) {
   //   document.body.style.background = `#${Math.floor(Math.random() * 16777215)}`;
   bgColor = `#` + Math.floor(Math.random() * 16777215).toString(16);
 
   document.body.style.background = bgColor;
   button.textContent = bgColor;
+  console.log(e);
 }
+
+// button.addEventListener("mousedown", change);
+// function changeButton() {
+//   //   document.body.style.background = `#${Math.floor(Math.random() * 16777215)}`;
+//   button = `#` + Math.floor(Math.random() * 16777215).toString(16);
+
+//   button.style.background = button;
+//   //   button.textContent = bgColor;
+//   console.log(e);
+// }
+
+// https://stackoverflow.com/questions/46716945/generate-random-background-color-by-clicking-on-link
